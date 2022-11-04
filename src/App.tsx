12174cad83dddcee479a977b491e9827c3863e7a
sym-dev <37952374+sym-dev@users.noreply.github.com>
2022-11-05 00:09:26 +0900

    darkmode

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,41 @@
-import { MantineProvider, Text } from "@mantine/core";
+import {
+  MantineProvider,
+  ColorSchemeProvider,
+  ColorScheme,
+  Text,
+} from "@mantine/core";
+import { useColorScheme, useLocalStorage } from "@mantine/hooks";
+import { useEffect } from "react";
 
 export default function App() {
+  const preferred = useColorScheme();
+  const [colorScheme, setColorScheme] = useLocalStorage<ColorScheme>({
+    key: "mantine-color-scheme",
+    defaultValue: "light",
+    getInitialValueInEffect: true,
+  });
+
+  const toggleColorScheme = (value?: ColorScheme) =>
+    setColorScheme(value || (colorScheme === "dark" ? "light" : "dark"));
+
+  useEffect(() => {
+    if (colorScheme !== preferred) toggleColorScheme(preferred);
+  }, [preferred]);
+
   return (
-    <MantineProvider
-      withGlobalStyles
-      withNormalizeCSS
-      withCSSVariables
+    <ColorSchemeProvider
+      colorScheme={colorScheme}
+      toggleColorScheme={toggleColorScheme}
     >
-      <Text>Welcome to Mantine!</Text>
-    </MantineProvider>
+      <MantineProvider
+        theme={{ colorScheme }}
+        withGlobalStyles
+        withNormalizeCSS
+        withCSSVariables
+      >
+        <Text>Welcome to Mantine!</Text>
+        <button onClick={() => toggleColorScheme()}>OMG</button>
+      </MantineProvider>
+    </ColorSchemeProvider>
   );
 }
