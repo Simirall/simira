8e8258604dbff0d944032b1a16da4bab074401b2
sym-dev <37952374+sym-dev@users.noreply.github.com>
2022-11-04 23:38:45 +0900

    linter+formatter

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,11 @@ import { MantineProvider, Text } from "@mantine/core";
 
 export default function App() {
   return (
-    <MantineProvider withGlobalStyles withNormalizeCSS withCSSVariables>
+    <MantineProvider
+      withGlobalStyles
+      withNormalizeCSS
+      withCSSVariables
+    >
       <Text>Welcome to Mantine!</Text>
     </MantineProvider>
   );
